01a5ac847c32740d0331e742f11c85644a40ee6f
g6714 <venelin.petkov@man.eu>
2018-06-17 13:07:47 +0200

    Added the yolo model

--- src/main/java/com/elm/model/yolo/Application.java
+++ src/main/java/com/elm/model/yolo/Application.java
@@ -1,14 +1,37 @@
 package com.elm.model.yolo;
 
+import java.io.File;
+import java.util.Random;
+
 import org.datavec.api.split.FileSplit;
-import org.datavec.image.recordreader.VideoRecordReader;
+import org.datavec.image.loader.NativeImageLoader;
+import org.datavec.image.recordreader.objdetect.ObjectDetectionRecordReader;
+import org.datavec.image.recordreader.objdetect.impl.SvhnLabelProvider;
 import org.deeplearning4j.datasets.datavec.RecordReaderDataSetIterator;
-import org.nd4j.linalg.dataset.DataSet;
-import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
+import org.deeplearning4j.datasets.fetchers.DataSetType;
+import org.deeplearning4j.datasets.fetchers.SvhnDataFetcher;
+import org.deeplearning4j.nn.api.OptimizationAlgorithm;
+import org.deeplearning4j.nn.conf.ConvolutionMode;
+import org.deeplearning4j.nn.conf.GradientNormalization;
+import org.deeplearning4j.nn.conf.WorkspaceMode;
+import org.deeplearning4j.nn.conf.inputs.InputType;
+import org.deeplearning4j.nn.conf.layers.ConvolutionLayer;
+import org.deeplearning4j.nn.conf.layers.objdetect.Yolo2OutputLayer;
+import org.deeplearning4j.nn.graph.ComputationGraph;
+import org.deeplearning4j.nn.transferlearning.FineTuneConfiguration;
+import org.deeplearning4j.nn.transferlearning.TransferLearning;
+import org.deeplearning4j.nn.weights.WeightInit;
+import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
+import org.deeplearning4j.util.ModelSerializer;
+import org.deeplearning4j.zoo.model.TinyYOLO;
+import org.nd4j.linalg.activations.Activation;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.dataset.api.preprocessor.ImagePreProcessingScaler;
+import org.nd4j.linalg.factory.Nd4j;
+import org.nd4j.linalg.learning.config.Adam;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.File;
 import java.io.IOException;
 
 public class Application {
@@ -16,27 +39,125 @@ public class Application {
     private static Logger log = LoggerFactory.getLogger(Application.class);
     private static String parentDir = "/Users/g6714/Data/autobahn/";
 
-    private static DataSetIterator loadData() throws IOException, InterruptedException{
-        FileSplit fileSplit = new FileSplit(new File(parentDir));
-        VideoRecordReader recordReader = new VideoRecordReader(224, 224);
-        recordReader.initialize(fileSplit);
-        return new RecordReaderDataSetIterator(recordReader, 256);
-    }
-
-    public static void main(String[] args) throws IOException, InterruptedException {
-        log.info("=== Kafka Video Streaming Application Example ===");
+    public static void main(String[] args) throws IOException {
+        log.info("=== Yolo Model Training ===");
         Arguments cliArgs = ArgumentParser.parseCliArguments(args);
 
-//        DataSetIterator dataIter = loadData();
-//
-//        int dataSetCounter = 0;
-//        while (dataIter.hasNext()) {
-//            DataSet ds = dataIter.next();
-//            String fileName = String.format("data/data_%05d.bin", dataSetCounter);
-//            log.info("Saving {} records to file: {}", ds.numExamples(), fileName);
-//            ds.save(new File(fileName));
-//            dataSetCounter++;
-//        }
+        // parameters matching the pretrained TinyYOLO model
+        int width = 416;
+        int height = 416;
+        int nChannels = 3;
+        int gridWidth = 13;
+        int gridHeight = 13;
 
+        // number classes (digits) for the SVHN datasets
+        int nClasses = 10;
+
+        // parameters for the Yolo2OutputLayer
+        int nBoxes = 5;
+        double lambdaNoObj = 0.5;
+        double lambdaCoord = 1.0;
+        double[][] priorBoxes = {{2, 5}, {2.5, 6}, {3, 7}, {3.5, 8}, {4, 9}};
+        double detectionThreshold = 0.5;
+
+        // parameters for the training phase
+        int batchSize = 10;
+        int nEpochs = 20;
+        double learningRate = 1e-4;
+        double lrMomentum = 0.9;
+
+        int seed = 123;
+        Random rng = new Random(seed);
+
+        SvhnDataFetcher fetcher = new SvhnDataFetcher();
+        File trainDir = fetcher.getDataSetPath(DataSetType.TRAIN);
+        File testDir = fetcher.getDataSetPath(DataSetType.TEST);
+
+
+        log.info("Load data...");
+
+        FileSplit trainData = new FileSplit(trainDir, NativeImageLoader.ALLOWED_FORMATS, rng);
+        FileSplit testData = new FileSplit(testDir, NativeImageLoader.ALLOWED_FORMATS, rng);
+
+        ObjectDetectionRecordReader recordReaderTrain = new ObjectDetectionRecordReader(height, width, nChannels,
+                gridHeight, gridWidth, new SvhnLabelProvider(trainDir));
+        recordReaderTrain.initialize(trainData);
+
+        ObjectDetectionRecordReader recordReaderTest = new ObjectDetectionRecordReader(height, width, nChannels,
+                gridHeight, gridWidth, new SvhnLabelProvider(testDir));
+        recordReaderTest.initialize(testData);
+
+        // ObjectDetectionRecordReader performs regression, so we need to specify it here
+        RecordReaderDataSetIterator train = new RecordReaderDataSetIterator(recordReaderTrain, batchSize, 1, 1, true);
+        train.setPreProcessor(new ImagePreProcessingScaler(0, 1));
+
+        RecordReaderDataSetIterator test = new RecordReaderDataSetIterator(recordReaderTest, 1, 1, 1, true);
+        test.setPreProcessor(new ImagePreProcessingScaler(0, 1));
+
+
+        ComputationGraph model;
+        String modelFilename = "model.zip";
+
+        if (new File(modelFilename).exists()) {
+            log.info("Load model...");
+
+            model = ModelSerializer.restoreComputationGraph(modelFilename);
+        } else {
+            log.info("Build model...");
+
+            ComputationGraph pretrained = (ComputationGraph) TinyYOLO.builder().build().initPretrained();
+            INDArray priors = Nd4j.create(priorBoxes);
+
+            FineTuneConfiguration fineTuneConf = new FineTuneConfiguration.Builder()
+                    .seed(seed)
+                    .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
+                    .gradientNormalization(GradientNormalization.RenormalizeL2PerLayer)
+                    .gradientNormalizationThreshold(1.0)
+                    .updater(new Adam.Builder().learningRate(learningRate).build())
+                    //.updater(new Nesterovs.Builder().learningRate(learningRate).momentum(lrMomentum).build())
+                    .l2(0.00001)
+                    .activation(Activation.IDENTITY)
+                    .trainingWorkspaceMode(WorkspaceMode.SEPARATE)
+                    .inferenceWorkspaceMode(WorkspaceMode.SEPARATE)
+                    .build();
+
+            model = new TransferLearning.GraphBuilder(pretrained)
+                    .fineTuneConfiguration(fineTuneConf)
+                    .removeVertexKeepConnections("conv2d_9")
+                    .addLayer("convolution2d_9",
+                            new ConvolutionLayer.Builder(1, 1)
+                                    .nIn(1024)
+                                    .nOut(nBoxes * (5 + nClasses))
+                                    .stride(1, 1)
+                                    .convolutionMode(ConvolutionMode.Same)
+                                    .weightInit(WeightInit.XAVIER)
+                                    .activation(Activation.IDENTITY)
+                                    .build(),
+                            "leaky_re_lu_8")
+                    .addLayer("outputs",
+                            new Yolo2OutputLayer.Builder()
+                                    .lambbaNoObj(lambdaNoObj)
+                                    .lambdaCoord(lambdaCoord)
+                                    .boundingBoxPriors(priors)
+                                    .build(),
+                            "convolution2d_9")
+                    .setOutputs("outputs")
+                    .build();
+
+            System.out.println(model.summary(InputType.convolutional(height, width, nChannels)));
+
+
+            log.info("Train model...");
+
+            model.setListeners(new ScoreIterationListener(1));
+            for (int i = 0; i < nEpochs; i++) {
+                train.reset();
+                while (train.hasNext()) {
+                    model.fit(train.next());
+                }
+                log.info("*** Completed epoch {} ***", i);
+            }
+            ModelSerializer.writeModel(model, modelFilename, true);
+        }
     }
 }
